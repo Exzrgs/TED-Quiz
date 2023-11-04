408578c346ae723f29e93f9d2b1b7be71f1b8599
unknown <koyamashu3@keio.jp>
2023-11-04 19:03:08 +0900

    スクロールの実装と問題生成ボタン実装

--- node18/front/src/content/Content.tsx
+++ node18/front/src/content/Content.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import { getBucket } from '@extend-chrome/storage';
 import { Radio, Group } from '@mantine/core';
 import RadioButton from './Radiobottun';
-import get_problems from '../app/translate'
+import {get_problems} from '../app/translate'
 interface MyBucket {
   targetLang: string;
 }
@@ -13,7 +13,7 @@ const Content = () => {
   //ここにapiからうけとったやつストレージからぶち込む
   const translateSelectedNumber = {'first':1,'second':2,'third':3,"fourth":4};
 //  const selectsFromGpt = get_problems()
-  const selectsFromGpt = [
+  let selectsFromGpt = [
       {"problem_statement":"statement",
       "answer_options": {'first':"answer1",'second':"answer2",'third':"answer3","fourth":"answer4"},
       "answer":1
@@ -34,6 +34,7 @@ const Content = () => {
       "answer_options": {'first':"answer1",'second':"answer2",'third':"answer3","fourth":"answer4"},
       "answer":1
       }]
+
   const [lang, setLang] = useState('EN');
   const [selects, setSelects] = useState(selectsFromGpt);
   const [answers, setAnswers] = useState({});
@@ -56,6 +57,13 @@ const handleSelect = (whichAnswer, value) => {
   setAnswers(prevAnswers => ({ ...prevAnswers, [whichAnswer]: value }));
 };
 
+//　問題を生成する関数
+const createProblems = () => {
+  let selectsFromGpt = get_problems();
+  console.log(selectsFromGpt);
+};
+
+
 // 正解を確認する関数
 const checkAnswers = () => {
   console.log(answers);
@@ -68,42 +76,36 @@ const checkAnswers = () => {
 
   // 正解数を計算
   let score = 0;
+  let correctLists = "";
   for(let i = 0;i<5;i++){
     if(correctAnswers[i].correct == translateSelectedNumber[correctAnswers[i].selected]){
       score++;
+      correctLists += (i+1).toFixed(0)+'、';
     }
   };
 
   // スコア表示などの処理
-  alert(`You scored ${score} out of ${correctAnswers.length}`);
+  alert(
+    `あなたのスコアは${score}/${correctAnswers.length}です。\nそのうち正解した問題は、` + correctLists+`です！`);
 };
 
+    return (
+      <div className="fixed z-[999] bottom-2 right-2 w-full max-w-md h-screen overflow-y-auto shadow-xl border-[1px] bg-black bg-opacity-50">
+                        <button onClick={createProblems}>問題生成</button>
+        <div className="flex justify-center mt-2 text-base">問題</div>
+        {selectsFromGpt.map((select, index) => (
+          <RadioButton 
+            key={index}
+            selects={selects} 
+            whichAnswer={index} 
+            onSelect={handleSelect} // onSelect プロップとして関数を渡す
+          />
+        ))}
+                <button onClick={checkAnswers}>Check Answers</button>
 
-
-
-  return (
-    <div className="fixed z-[999] bottom-2 right-2 h-screen shadow-xl border-[1px] bg-black bg-opacity-50">
-      <div className="flex justify-center mt-2 text-base">Content</div>
-      {/* <Counter /> */}
-      {/* <RadioButton selects={selects} whichAnswer={0}/>
-      <RadioButton selects={selects} whichAnswer={1}/>
-      <RadioButton selects={selects} whichAnswer={2}/>
-      <RadioButton selects={selects} whichAnswer={3}/>
-      <RadioButton selects={selects} whichAnswer={4}/> */}
-    <button onClick={checkAnswers}>Check Answers</button>
-
-      {selectsFromGpt.map((select, index) => (
-      <RadioButton 
-        key={index}
-        selects={selects} 
-        whichAnswer={index} 
-        onSelect={handleSelect} // onSelect プロップとして関数を渡す
-      />
-    ))}
-
-    </div>
+      </div>
     );
-
+    
 return (
     <>
     <div className="fixed z-[999] bottom-2 right-2 w-1/4 h-screen shadow-xl border-[1px] bg-black bg-opacity-20">
